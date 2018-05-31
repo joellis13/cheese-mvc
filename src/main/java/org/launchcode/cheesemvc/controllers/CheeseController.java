ece01958b435ed42e4b022834bff1b5cae600835
Jonathan Ellis <joellis13@yahoo.com>
2018-05-30 19:54:27 -0500

    Take 2, first commit

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,19 +1,44 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
-
-
+/**
+ * Created by Jonathan Ellis
+ */
 @Controller
+@RequestMapping("cheese") // Redirects all method mapping to /cheese
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
 
+    // Request path: /cheese
     @RequestMapping(value = "")
-    public String index() {
-        return "index";
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses!!");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+
+        // redirect to /cheese (home)
+        return "redirect:";
     }
 
 }
